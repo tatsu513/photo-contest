7dc79e7f0763cfb92582fa805ef7e5b969baa8e4
tatsu513 <0285mashiko@gmail.com>
2021-10-19 00:13:46 +0900

    youtube

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,7 @@ import Ichioshi from "@/components/Ichioshi";
 import Yushusakuhin from "@/components/Yushusakuhin";
 import Posts from "@/components/Posts";
 import HowToApply from "@/components/Apply";
+import Torikatakouza from "@/components/Torikatakouza";
 
 const IndexPage = () => (
   <>
@@ -16,6 +17,7 @@ const IndexPage = () => (
     <Yushusakuhin />
     <Posts />
     <HowToApply />
+    <Torikatakouza />
   </>
 );
 
