5ea5d28d525c1aee11ce79db96d603a54954b45c
tatsu513 <0285mashiko@gmail.com>
2021-10-19 17:19:55 +0900

    topimage animation

--- src/components/TopImage.tsx
+++ src/components/TopImage.tsx
@@ -1,12 +1,36 @@
-import React from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import styles from "styles/modules/TopImage.module.scss";
 import Image from "next/image";
 import topImageSp from "images/top_image_sp.svg";
 import naminamiSp from "images/naminami-shiro_sp.svg";
 
 const TopImage = () => {
+  const [currentImage, setCurrentImage] = useState(1);
+  const bgClassName = useCallback(() => {
+    switch (currentImage) {
+      case 1:
+        return "bg1";
+      case 2:
+        return "bg2";
+      case 3:
+        return "bg3";
+      case 4:
+        return "bg4";
+    }
+  }, [currentImage]);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (currentImage >= 4) {
+        setCurrentImage(1);
+      } else {
+        setCurrentImage(() => currentImage + 1);
+      }
+    }, 6000);
+    return () => clearInterval(interval);
+  }, [currentImage]);
   return (
-    <div className={styles.topImageBox}>
+    <div className={`${styles.topImageBox} ${styles[bgClassName()]}`}>
       <div className={styles.image}>
         <Image
           src={topImageSp}
