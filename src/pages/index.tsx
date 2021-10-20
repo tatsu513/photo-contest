4df5d8392622fdfd3c7f65a034fb197eb06e2b15
tatsu513 <0285mashiko@gmail.com>
2021-10-21 01:00:52 +0900

    lg用の募集要項コンポーネント

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,21 +8,28 @@ import HowToApply from "@/components/Apply";
 import Torikatakouza from "@/components/Torikatakouza";
 import Boshuyoukou from "@/components/Boshuyoukou";
 import Footer from "@/components/Footer";
+import { ContextData } from "./_app";
+import { useContext } from "react";
+import BoshuyoukouLg from "@/components/BoshuyoukouLg";
 
-const IndexPage = () => (
-  <>
-    <Head>
-      <title>流山カレンダーフォトコンテスト</title>
-    </Head>
-    <TopImage />
-    <Ichioshi />
-    <Yushusakuhin />
-    <Posts />
-    <HowToApply />
-    <Torikatakouza />
-    <Boshuyoukou />
-    <Footer />
-  </>
-);
+const IndexPage = () => {
+  const ctx = useContext(ContextData);
+  const isLg = ctx.windowWidth > 1025;
+  return (
+    <>
+      <Head>
+        <title>流山カレンダーフォトコンテスト</title>
+      </Head>
+      <TopImage />
+      <Ichioshi />
+      <Yushusakuhin />
+      <Posts />
+      <HowToApply />
+      <Torikatakouza />
+      {isLg ? <BoshuyoukouLg /> : <Boshuyoukou />}
+      <Footer />
+    </>
+  );
+};
 
 export default IndexPage;
