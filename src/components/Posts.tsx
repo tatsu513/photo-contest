e79363c63a0f5779a3f56261508a7a6caa842810
tatsu513 <0285mashiko@gmail.com>
2021-10-21 18:50:45 +0900

    もっとみるボタンを実装

--- src/components/Posts.tsx
+++ src/components/Posts.tsx
@@ -3,8 +3,69 @@ import Image from "next/image";
 import sectionTitle from "images/section_title03_sp.svg";
 import anotherIcon from "images/anotherIcon.svg";
 import PrimaryButton from "./buttons/PrimaryButton";
+import { useState } from "react";
 
 const Posts = () => {
+  const [showNum, setShowNum] = useState(6);
+  const addShowNum = () => setShowNum((prevState) => prevState + 6);
+
+  const dammys = [
+    {
+      caption:
+        "１２３４５６７８９０１２３４５６７８９０１２３４５６７８９０",
+      url: "https://lazesoftware.com/tool/dummyimg/output?date=20211018&id=47070197f4cfaa1a9979f19f686dea41dc56a7b0&type=jpeg",
+    },
+    {
+      caption:
+        "１２３４５６７８９０１２３４５６７８９０１２３４５６７８９０",
+      url: "https://lazesoftware.com/tool/dummyimg/output?date=20211018&id=47070197f4cfaa1a9979f19f686dea41dc56a7b0&type=jpeg",
+    },
+    {
+      caption:
+        "１２３４５６７８９０１２３４５６７８９０１２３４５６７８９０",
+      url: "https://lazesoftware.com/tool/dummyimg/output?date=20211018&id=47070197f4cfaa1a9979f19f686dea41dc56a7b0&type=jpeg",
+    },
+    {
+      caption:
+        "１２３４５６７８９０１２３４５６７８９０１２３４５６７８９０",
+      url: "https://lazesoftware.com/tool/dummyimg/output?date=20211018&id=47070197f4cfaa1a9979f19f686dea41dc56a7b0&type=jpeg",
+    },
+    {
+      caption:
+        "１２３４５６７８９０１２３４５６７８９０１２３４５６７８９０",
+      url: "https://lazesoftware.com/tool/dummyimg/output?date=20211018&id=47070197f4cfaa1a9979f19f686dea41dc56a7b0&type=jpeg",
+    },
+    {
+      caption:
+        "１２３４５６７８９０１２３４５６７８９０１２３４５６７８９０",
+      url: "https://lazesoftware.com/tool/dummyimg/output?date=20211018&id=47070197f4cfaa1a9979f19f686dea41dc56a7b0&type=jpeg",
+    },
+    {
+      caption:
+        "１２３４５６７８９０１２３４５６７８９０１２３４５６７８９０",
+      url: "https://lazesoftware.com/tool/dummyimg/output?date=20211018&id=47070197f4cfaa1a9979f19f686dea41dc56a7b0&type=jpeg",
+    },
+    {
+      caption:
+        "１２３４５６７８９０１２３４５６７８９０１２３４５６７８９０",
+      url: "https://lazesoftware.com/tool/dummyimg/output?date=20211018&id=47070197f4cfaa1a9979f19f686dea41dc56a7b0&type=jpeg",
+    },
+    {
+      caption:
+        "１２３４５６７８９０１２３４５６７８９０１２３４５６７８９０",
+      url: "https://lazesoftware.com/tool/dummyimg/output?date=20211018&id=47070197f4cfaa1a9979f19f686dea41dc56a7b0&type=jpeg",
+    },
+    {
+      caption:
+        "１２３４５６７８９０１２３４５６７８９０１２３４５６７８９０",
+      url: "https://lazesoftware.com/tool/dummyimg/output?date=20211018&id=47070197f4cfaa1a9979f19f686dea41dc56a7b0&type=jpeg",
+    },
+  ];
+
+  const customData = dammys
+    .map((dammy, i) => (i < showNum ? dammy : null))
+    .filter((data) => data);
+
   return (
     <div className={styles.postsBox}>
       <section className={styles.titleBox}>
@@ -16,7 +77,7 @@ const Posts = () => {
         </p>
         <div className={styles.posts}>
           <ul className={styles.postBox}>
-            {[...Array(4)].map((_, i) => (
+            {customData.map((dammy, i) => (
               <li key={i} className={styles.post}>
                 <div className={styles.anotherIcon}>
                   <Image
@@ -26,22 +87,18 @@ const Posts = () => {
                 </div>
                 <div className={styles.postImageBox}>
                   <Image
-                    src={
-                      "https://lazesoftware.com/tool/dummyimg/output?date=20211018&id=47070197f4cfaa1a9979f19f686dea41dc56a7b0&type=jpeg"
-                    }
+                    src={dammy.url}
                     alt="サンプル"
                     width={1000}
                     height={1000}
                   />
                 </div>
-                <p className={styles.postText}>
-                  １２３４５６７８９０１２３４５６７８９０１２３４５６７８９０
-                </p>
+                <p className={styles.postText}>{dammy.caption}</p>
               </li>
             ))}
           </ul>
         </div>
-        <PrimaryButton text={"もっと見る"} />
+        <PrimaryButton text={"もっと見る"} onClick={addShowNum} />
       </section>
       <div className="namiBox midori" />
     </div>
