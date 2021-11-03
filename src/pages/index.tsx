1482a43821cb14fe9fa01cf6561a555bcd142b68
tatsu513 <0285mashiko@gmail.com>
2021-11-03 15:13:32 +0900

    hash id を定数化

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -78,15 +78,10 @@ const IndexPage: React.VFC<Props> = ({ postData }) => {
 };
 
 export const getStaticProps = async () => {
-  const instagramId = process.env.NEXT_PUBLIC_HASH_ID;
+  const instagramId = process.env.NEXT_PUBLIC_INSTGRAM_ID;
   const query = encodeURI("マキアート");
   const accessToken = process.env.NEXT_PUBLIC_ACCESS_TOKEN;
-  const idSearchUrl = `https://graph.facebook.com/ig_hashtag_search?user_id=${instagramId}&q=${query}&access_token=${accessToken}`;
-
-  const res = await fetch(idSearchUrl, { method: "GET" });
-  const json = await res.json();
-  const hashId = json.data[0].id;
-
+  const hashId = process.env.NEXT_PUBLIC_HASH_ID;
   const getDataUrl = `https://graph.facebook.com/${hashId}/top_media?user_id=${instagramId}&q=${query}&access_token=${accessToken}&fields=id,media_type,media_url,permalink,like_count,comments_count,caption,timestamp,children{id,media_url}&limit=10`;
 
   const dataRes = await fetch(getDataUrl, { method: "GET" });
