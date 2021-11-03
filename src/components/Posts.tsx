4ea5b16743ec081745d187450adaa49ba8d1b928
tatsu513 <0285mashiko@gmail.com>
2021-11-03 22:51:56 +0900

    text3

--- src/components/Posts.tsx
+++ src/components/Posts.tsx
@@ -72,9 +72,11 @@ const Posts: React.VFC<Props> = ({ postData }) => {
     },
   ];
 
-  // const customData = posts
-  //   .map((dammy, i) => (i < showNum ? dammy : null))
-  //   .filter((data) => data);
+  const customData = posts
+    ? posts
+        .map((dammy, i) => (i < showNum ? dammy : null))
+        .filter((data) => data)
+    : [];
 
   return (
     <div className={styles.postsBox}>
@@ -86,7 +88,7 @@ const Posts: React.VFC<Props> = ({ postData }) => {
           Instagramに投稿された作品から、ピックアップしてご紹介！
         </p>
         <div className={styles.posts}>
-          {/* <ul
+          <ul
             className={`${styles.postBox} ${
               customData.length % 3 === 2 && styles.remainder
             }`}
@@ -118,7 +120,7 @@ const Posts: React.VFC<Props> = ({ postData }) => {
                 </Link>
               </li>
             ))}
-          </ul> */}
+          </ul>
         </div>
         <PrimaryButton text={"もっと見る"} onClick={addShowNum} />
       </section>
