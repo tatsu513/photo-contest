9eb5f14a7a63dbe524c68ba7622b2d057dc38111
tatsu513 <0285mashiko@gmail.com>
2021-11-09 23:32:07 +0900

    不要を削除

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,14 +5,6 @@ const _document = () => {
     <Html lang="ja">
       <Head />
       <body>
-        <div id="fb-root"></div>
-        <script
-          async
-          defer
-          crossOrigin="anonymous"
-          src="https://connect.facebook.net/ja_JP/sdk.js#xfbml=1&version=v12.0&appId=2995311247424073&autoLogAppEvents=1"
-          nonce="NirHvNso"
-        />
         <Main />
         <NextScript />
       </body>
